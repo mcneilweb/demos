acab7cf2969f3c8123f31cacd644050e7552cbe2
Johnny McNeil <johnnymmcneil@gmail.com>
2024-02-03 15:29:59 -0700

    breaking changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [lit()],
 });
